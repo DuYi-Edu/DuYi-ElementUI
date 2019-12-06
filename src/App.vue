2b6fe016c7de4cb83836cddbcdcc004132826be8
shan0419 <shan041999@163.com>
2019-12-06 16:26:59 +0800

    7.b.默认按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <el-button>默认按123钮</el-button>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
--- src/components/element/button/src/Button.vue
+++ src/components/element/button/src/Button.vue
@@ -1,5 +1,10 @@
 <template>
-  <button>
+  <button
+    class="el-button"
+    :class="[
+      type ? `el-button--${type}` : '',
+    ]"
+  >
     <slot></slot>
   </button>
 </template>
@@ -7,5 +12,9 @@
 <script>
 export default {
   name: 'ElButton',
+
+  props: {
+    type: String,
+  },
 };
 </script>
