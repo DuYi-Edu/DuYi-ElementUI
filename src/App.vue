c7839ded098ac521d91fb725c56c346e6aada7d5
shan0419 <shan041999@163.com>
2019-12-07 14:34:17 +0800

    7.d.圆角按钮-圆形按钮-禁用状态

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,45 @@
       <el-button type="warning">警告按钮</el-button>
       <el-button type="danger">危险按钮</el-button>
     </el-row> -->
-    <el-row>
+    <!-- <el-row>
       <el-button plain>朴素按钮</el-button>
       <el-button type="primary" plain>主要按钮</el-button>
       <el-button type="success" plain>成功按钮</el-button>
       <el-button type="info" plain>信息按钮</el-button>
       <el-button type="warning" plain>警告按钮</el-button>
       <el-button type="danger" plain>危险按钮</el-button>
+    </el-row> -->
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+    <el-row>
+      <el-button disabled>默认按钮</el-button>
+      <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="success" disabled>成功按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="warning" disabled>警告按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
+    </el-row>
+    <el-row>
+      <el-button plain disabled>朴素按钮</el-button>
+      <el-button type="primary" plain disabled>主要按钮</el-button>
+      <el-button type="success" plain disabled>成功按钮</el-button>
+      <el-button type="info" plain disabled>信息按钮</el-button>
+      <el-button type="warning" plain disabled>警告按钮</el-button>
+      <el-button type="danger" plain disabled>危险按钮</el-button>
     </el-row>
   </div>
 </template>
@@ -29,37 +61,7 @@ export default {
 </script>
 
 <style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
+  .el-row {
+    margin-bottom: 20px;
   }
 </style>
--- src/components/element/button/src/Button.vue
+++ src/components/element/button/src/Button.vue
@@ -3,9 +3,15 @@
     class="el-button"
     :class="[
       type ? `el-button--${type}` : '',
-      plain ? 'is-plain' : '',
+      {
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+      }
     ]"
   >
+    <i :class="icon" v-if="icon" />
     <slot></slot>
   </button>
 </template>
@@ -17,7 +23,10 @@ export default {
   props: {
     type: String,
     plain: Boolean,
-
+    round: Boolean,
+    circle: Boolean,
+    icon: String,
+    disabled: Boolean,
   },
 };
 </script>
