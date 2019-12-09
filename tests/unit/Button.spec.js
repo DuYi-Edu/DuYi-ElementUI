ad716f8205a82f627dd6ad405b243c0bcc077a12
shan0419 <shan041999@163.com>
2019-12-09 20:57:28 +0800

    7.h.测试Button组件

---
+++ tests/unit/Button.spec.js
@@ -0,0 +1,98 @@
+import Button from '@element/button';
+import { shallowMount } from '@vue/test-utils';
+
+describe('Button.vue', () => {
+  let wrapper;
+  let buttonEle;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Button);
+    buttonEle = wrapper.vm.$el;
+  });
+  it('size', () => {
+    wrapper.setProps({
+      size: 'medium',
+    });
+
+    expect(buttonEle.classList).toContain('el-button--medium');
+  });
+
+  it('type', () => {
+    wrapper.setProps({
+      type: 'primary',
+    });
+
+    expect(buttonEle.classList).toContain('el-button--primary');
+  });
+
+  it('plain', () => {
+    wrapper.setProps({
+      plain: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-plain');
+  });
+
+  it('round', () => {
+    wrapper.setProps({
+      round: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-round');
+  });
+
+  it('circle', () => {
+    wrapper.setProps({
+      circle: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-circle');
+  });
+
+  it('loading', () => {
+    wrapper.setProps({
+      loading: true,
+    });
+
+    expect(wrapper.html()).toContain('<i class="el-icon-loading"></i>');
+  });
+
+  it('disabled', () => {
+    wrapper.setProps({
+      disabled: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-disabled');
+  });
+
+  it('icon', () => {
+    wrapper.setProps({
+      icon: 'el-icon-edit',
+    });
+
+    expect(wrapper.html()).toContain('<i class="el-icon-edit"></i>');
+  });
+
+  it('autofocus', () => {
+    wrapper.setProps({
+      autofocus: true,
+    });
+
+    expect(wrapper.attributes('autofocus')).toEqual('autofocus');
+  });
+
+  it('nativeType', () => {
+    wrapper.setProps({
+      nativeType: 'submit',
+    });
+
+    expect(wrapper.attributes('type')).toEqual('submit');
+  });
+
+  it('click', () => {
+    const buttonVm = wrapper.find(Button);
+    wrapper.find('button').trigger('click');
+    expect(buttonVm.emitted().click).toBeTruthy();
+    expect(wrapper.emitted().click).toBeTruthy();
+  });
+});
