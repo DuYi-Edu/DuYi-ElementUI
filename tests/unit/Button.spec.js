f0cf1840000c9bbbcec9f8fb243374b656ef0017
shan0419 <shan041999@163.com>
2019-12-10 12:44:43 +0800

    7.h.测试Button组件

---
+++ tests/unit/Button.spec.js
@@ -0,0 +1,100 @@
+import Button from '@element/button';
+import { shallowMount } from '@vue/test-utils';
+
+describe('Button.vue', () => {
+  let wrapper;
+  let buttonEle;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Button);
+    buttonEle = wrapper.vm.$el;
+  });
+  it('create', () => {
+    expect(buttonEle.classList).toContain('el-button');
+  });
+
+  it('size', () => {
+    wrapper.setProps({
+      size: 'mini',
+    });
+
+    expect(buttonEle.classList).toContain('el-button--mini');
+  });
+
+  it('type', () => {
+    wrapper.setProps({
+      type: 'primary',
+    });
+
+    expect(buttonEle.classList).toContain('el-button--primary');
+  });
+
+  it('plain', () => {
+    wrapper.setProps({
+      plain: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-plain');
+  });
+
+  it('round', () => {
+    wrapper.setProps({
+      round: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-round');
+  });
+
+  it('circle', () => {
+    wrapper.setProps({
+      circle: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-circle');
+  });
+
+  it('disabled', () => {
+    wrapper.setProps({
+      disabled: true,
+    });
+
+    expect(buttonEle.classList).toContain('is-disabled');
+  });
+
+  it('loading', () => {
+    wrapper.setProps({
+      loading: true,
+    });
+
+    expect(wrapper.html()).toContain('<i class="el-icon-loading"></i>');
+  });
+
+  it('icon', () => {
+    wrapper.setProps({
+      icon: 'el-icon-edit',
+    });
+
+    expect(wrapper.html()).toContain('<i class="el-icon-edit"></i>');
+  });
+
+  it('autofocus', () => {
+    wrapper.setProps({
+      autofocus: true,
+    });
+
+    expect(wrapper.attributes('autofocus')).toEqual('autofocus');
+  });
+
+  it('native-type', () => {
+    wrapper.setProps({
+      nativeType: 'submit',
+    });
+
+    expect(wrapper.attributes('type')).toEqual('submit');
+  });
+
+  it('click', () => {
+    wrapper.trigger('click');
+    expect(wrapper.emitted().click).toBeTruthy();
+  });
+});
